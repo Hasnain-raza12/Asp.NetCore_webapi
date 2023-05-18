d46f149320b6fa189b7dc1a62c08b5ec0c777a4d
Muhammad Hasnain Raza <70515736+Hasnain-raza12@users.noreply.github.com>
2023-05-18 20:05:01 +0500

    middleware

---
+++ web_api/web_api/Middleware/MyMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Amazon.Runtime.Internal.Transform;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace web_api.Middleware
+{
+    public class MyMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (!context.Request.Headers.ContainsKey("Authorization"))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("UnAuthorized");
+
+                return;
+            }
+            var header = context.Request.Headers["Authorization"].ToString();
+            var encodedCre = header.Substring(6);
+            var creds =Encoding.UTF8.GetString(Convert.FromBase64String(encodedCre));
+            string[] uidpwd = creds.Split(':');
+
+            var uid = uidpwd[0];
+            var upass = uidpwd[1];
+
+            if (uid=="Hasnain" && upass=="pass")
+            {
+                await next(context);
+            }
+            else
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("UnAuthorized");
+                return;
+            }
+
+
+        }
+
+       
+    }
+    public static class CustomMiddlewareExtension
+    {
+
+        public static IApplicationBuilder MyMiddleware(this IApplicationBuilder app)
+        {
+
+            return app.UseMiddleware<MyMiddleware>();
+        }
+    }
+}
--- web_api/web_api/Program.cs
+++ web_api/web_api/Program.cs
@@ -1,7 +1,9 @@
 using Api.Common;
-using Api.Common.Models;    
+using Api.Common.Models;
+using web_api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -11,11 +13,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMongo();
 builder.Services.AddRepositry<Customer>("customers");
+
+builder.Services.AddTransient<MyMiddleware>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -23,7 +28,31 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.MyMiddleware();
+//app.UseWhen(context => context.Request.Query.ContainsKey("IsAuthorized") && context.Request.Query["IsAuthorized"] == "true",
 
+//   app =>
+//   {
+//       app.Use(async (context, next) =>
+//       {
+//           await context.Response.WriteAsync("Conditional middleware");
+//           await next(context);
+//       });
+
+//   });
 app.MapControllers();
 
+//app.UseMiddleware<MyMiddleware>();
+////app.MyMiddleware();
+//app.UseWhen(context => context.Request.Query.ContainsKey("IsAuthorized") && context.Request.Query["IsAuthorized"]==true,
+
+//   app =>
+//   {
+//      app.Use(async (context, next) =>
+//      {
+//         await context.Response.WriteAsync("Conditional middleware");
+//         await next(context);
+//      });
+
+//   });
 app.Run();
